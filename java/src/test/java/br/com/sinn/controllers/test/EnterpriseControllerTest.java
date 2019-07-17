b004d054a8030ad3e543d4c41fabfa9395c8e16d
Taian Jesus Silva <tjesussi@everis.com>
2019-07-17 18:32:41 -0300

    Teste unitario

--- java/src/test/java/br/com/sinn/controllers/test/EnterpriseControllerTest.java
+++ java/src/test/java/br/com/sinn/controllers/test/EnterpriseControllerTest.java
@@ -13,16 +13,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -30,11 +26,9 @@ import br.com.sinn.controller.EnterpriseController;
 import br.com.sinn.domain.Enterprise;
 import br.com.sinn.services.ServiceEnterprise;
 
-
-
-@RunWith(SpringJUnit4ClassRunner.class)
-
 public class EnterpriseControllerTest {
+	
+	private static final String[] CPF = {"89943227000184","61192416000143"};
 	
 	private MockMvc mockMvc;
 	
@@ -44,33 +38,35 @@ public class EnterpriseControllerTest {
 	@InjectMocks
 	private EnterpriseController mockController;
 	
-	@Rule
-	private ExpectedException thrown = ExpectedException.none();
-	
 	@Before
 	public void init() {
 		MockitoAnnotations.initMocks(this);
 		mockMvc = MockMvcBuilders.standaloneSetup(mockController).build();
 	}
 	
-	@Test
-	public void findAllSuccessTest() throws Exception{
-		List<Enterprise> obj = Arrays.asList(getData(), getData());
-		Mockito.when(mockService.findAll()).thenReturn(obj);
-		mockMvc.perform(get("/enterprises"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-			//.andExpect(jsonPath("$", hasSize(2)))
-			.andExpect(jsonPath("$[0].id", is(1)))
-			.andExpect(jsonPath("$[0].username", is("Teste")))
-			.andExpect(jsonPath("$[0].id", is(2)))
-			.andExpect(jsonPath("$[0].username", is("Teste")));
-		verify(mockService, times(1)).findAll();
-		verifyNoMoreInteractions(mockService);		
-	}
-	
-	private Enterprise getData() {
-		return new Enterprise(1, "Teste", "12345678900123", "Teste");
-	}
-		 
+//	@Test
+//	public void findAllSuccessTest() throws Exception{
+////		List<Enterprise> obj = Arrays.asList(getData(), getData());
+//		Mockito.when(mockService.findAll()).thenReturn(obj);
+//		mockMvc.perform(get("/enterprises"))
+//			.andExpect(status().isOk())
+//			.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+//			//.andExpect(jsonPath("$", hasSize(2)))
+//			.andExpect(jsonPath("$[0].id", is(1)))
+//			.andExpect(jsonPath("$[0].nome_empresa", is("Teste")))
+//			.andExpect(jsonPath("$[0].cnpj_empresa", is("12345678900123")))
+//			.andExpect(jsonPath("$[0].dono_empresa", is("Teste")))
+//			.andExpect(jsonPath("$[0].id", is(2)))
+//			.andExpect(jsonPath("$[0].nome_empresa", is("Teste")))
+//			.andExpect(jsonPath("$[0].cnpj_empresa", is("12345678900123")))
+//			.andExpect(jsonPath("$[0].dono_empresa", is("Teste")));
+//		verify(mockService, times(1)).findAll();
+//		verifyNoMoreInteractions(mockService);		
+//	}
+//	
+//	private Enterprise getData() {
+//		for()
+//		return new Enterprise(1, "Teste", "12345678900123", "Teste");
+//	}
+//		 
 }
